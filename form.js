d96f3082336956ba1beebbfae6af8b719b235e27
hkarthikraj01 <53332719+hkarthikraj01@users.noreply.github.com>
2020-11-29 15:01:00 +0530

    Update form.js

--- form.js
+++ form.js
@@ -15,6 +15,39 @@ var firebaseConfig = {
   // update firestore settings
   db.settings({ timestampsInSnapshots: true });
 
+window.onload=function () {
+  render();
+};
+function render() {
+    window.recaptchaVerifier=new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    recaptchaVerifier.render();
+}
+function phoneAuth() {
+    //get the number
+    var number=document.getElementById('phone').value;
+    //phone number authentication function of firebase
+    //it takes two parameter first one is number,,,second one is recaptcha
+    firebase.auth().signInWithPhoneNumber(number,window.recaptchaVerifier).then(function (confirmationResult) {
+        //s is in lowercase
+        window.confirmationResult=confirmationResult;
+        coderesult=confirmationResult;
+        console.log(coderesult);
+        alert("Message sent");
+    }).catch(function (error) {
+        alert(error.message);
+    });
+}
+function codeverify() {
+    var code=document.getElementById('verificationCode').value;
+    coderesult.confirm(code).then(function (result) {
+        alert("Successfully registered");
+        var user=result.user;
+        console.log(user);
+    }).catch(function (error) {
+        alert(error.message);
+    });
+}
+
   gs=()=>{
   base_provider = new firebase.auth.GoogleAuthProvider()
       firebase.auth().signInWithPopup(base_provider).then(function(result){
